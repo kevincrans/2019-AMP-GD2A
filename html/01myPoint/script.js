80f1f425b4bf1335e95e70fca966a147c3c4cfca
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-09 11:25:44 +0200

    01 my point

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,9 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let A = new Point(new Vector2d(100,100),100,"rgba(100,100,100,0.5)");
+A.draw(context)
+
+let B = new Point(new Vector2d(500,400),200,"rgba(200,200,200,0.5)");
+B.draw(context);
