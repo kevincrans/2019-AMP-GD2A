af33a635d9f92cd0a39817c0bc3c943b52e260df
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-09 12:12:36 +0200

    my point with labels

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -7,8 +7,30 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(100,100),100,"rgba(100,100,100,0.5)");
-A.draw(context)
+let points = [];
 
-let B = new Point(new Vector2d(500,400),200,"rgba(200,200,200,0.5)");
-B.draw(context);
+
+function animate() {
+  context.clearRect(0,0,width,height);
+  requestAnimationFrame(animate);
+  let color = "rgb(" + getRandom(255) + "," + getRandom(255) + "," + getRandom(255) + ")"
+  let A = new Point(new Vector2d(getRandom(width),getRandom(height)),10,color,"hallos");
+  points.push(A);
+
+  for(let i = 0; i<points.length; i++){
+    points[i].label = i;
+    points[i].radius++;
+    points[i].draw(context);
+    if(points[i].radius > 100){
+      points.splice(i,1);
+    }
+  }
+}
+
+animate()
+
+
+function getRandom(max){
+  let ans = Math.floor(Math.random()*max);
+  return ans;
+}
