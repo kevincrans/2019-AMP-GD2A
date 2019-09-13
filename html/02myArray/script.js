aee59ead8133b1a23f25cb98b8ac9e9ef3b17fc8
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-13 15:57:26 +0200

    02_02

--- html/02myArray/script.js
+++ html/02myArray/script.js
@@ -6,3 +6,17 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let A = new Point(new Vector2d(200,200),100,"red");
+A.draw(context);
+
+window.addEventListener('click',(evt)=>{
+  let mouseVector = new Vector2d(evt.clientX,evt.clientY);
+  let differenceMousePoint = new Vector2d(0,0);
+
+  console.log(mouseVector);
+
+  differenceMousePoint.differenceVector(A.position,mouseVector);
+  console.log(differenceMousePoint)
+
+});
