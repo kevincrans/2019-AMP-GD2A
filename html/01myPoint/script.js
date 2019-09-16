c7f2988cefc0bdc2098cdaf4f5224988bd4466a0
kevincrans <kcrans43@gmail.com>
2019-09-16 12:15:33 +0200

    halloharry

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -10,29 +10,18 @@ canvas.height = height;
 let points = [];
 let counter = 0;
 
+let A = new Point(new Vector2d(500, 500), 200, "rgb(255, 0, 127)", "pointharry");
+
+
+A.draw(context);
 
 function animate() {
-  context.clearRect(0,0,width,height);
-  requestAnimationFrame(animate);
-  let color = "rgba(" + getRandom(255) + "," + getRandom(255) + "," + getRandom(255) + "," + 0.3 + ")"
-  let A = new Point(new Vector2d(getRandom(width),getRandom(height)),10,color,"hallos");
-  A.label = counter;
-  counter++;
-  points.push(A);
-
-  for(let i = 0; i<points.length; i++){
-    points[i].radius+= 0.4;
-    points[i].draw(context);
-    if(points[i].radius > 100){
-      points.splice(i,1);
-    }
-  }
+  
 }
 
 animate()
 
 
 function getRandom(max){
-  let ans = Math.floor(Math.random()*max);
-  return ans;
+
 }
